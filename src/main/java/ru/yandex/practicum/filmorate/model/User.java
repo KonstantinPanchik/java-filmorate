7798952fee8a78712483df9536eecb86c879ea90
Konstantin <tuder.muder@yandex.ru>
2023-11-05 15:28:35 +0400

    del: unusedimport

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,7 +6,6 @@ import lombok.NonNull;
 import ru.yandex.practicum.filmorate.validators.NotSpace;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
