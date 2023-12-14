6eaa651a5de68c80b699791a7d3c40c890d47afe
Konstantin <tuder.muder@yandex.ru>
2023-12-14 22:26:56 +0400

    Задание выполнено ожидает ревью

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 
